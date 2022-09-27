5355980c8cbe093b5ffa167ebe988a89f1371858
HARSH <theharsh1210@gmail.com>
2022-09-28 01:07:58 +0530

    firstscreen

--- app/src/main/java/com/example/goldenhour/MainActivity.kt
+++ app/src/main/java/com/example/goldenhour/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.goldenhour
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this@MainActivity, home::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
